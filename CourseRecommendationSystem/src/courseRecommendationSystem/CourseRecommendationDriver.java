a4052a1415e5e86013b47d69aaa2a0d9ccc089bc
Felix404-student <justhbrown@gmail.com>
2020-05-30 14:16:50 -0400

    Adding stub driver with welcome message

---
+++ CourseRecommendationSystem/src/courseRecommendationSystem/CourseRecommendationDriver.java
@@ -0,0 +1,10 @@
+package courseRecommendationSystem;
+
+public class CourseRecommendationDriver {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Course Recommendation System!\n\nWould you like to log in as a:");
+		System.out.println("(1)\tStudent\n(2)\tAdvisor\n(3)\tAdmin\n\nPlease select your choice by number:");
+	}
+
+}
