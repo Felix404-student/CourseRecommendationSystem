bae7bddbaeee7ac6ec3559eba59afc75da5955e5
lulugirrl <lulugirrl@192.168.1.21>
2020-06-01 16:56:30 -0400

    deleted irrelevant file

---
+++ src/classes/Menu.java
@@ -0,0 +1,9 @@
+package classes;
+/**
+ * creates an interface that will have child classes of menus specific to each user 
+ * @author natmonz
+ */
+public interface Menu {
+	public void runMenu(); 
+	public void displayMenuOption(); 
+}
