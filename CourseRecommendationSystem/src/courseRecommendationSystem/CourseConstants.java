c2bacc8bacaa8cf983852c2a10abd7456e6e4b7d
Felix404-student <justhbrown@gmail.com>
2020-06-02 13:15:02 -0400

    Combining Dongyu's files to master

---
+++ CourseRecommendationSystem/src/courseRecommendationSystem/CourseConstants.java
@@ -0,0 +1,13 @@
+package courseRecommendationSystem;
+
+public abstract class CourseConstants {
+	public static final String FILE_NAME = "src/json1.json";
+	public static final String NAME = "name";
+	public static final String CODE = "courseCode";
+	public static final String CREDITS = "courseCredit";
+	public static final String PREREQ = "prerequisites";
+	public static final String COREQ = "corequisite";
+	public static final String COURSEOFFERINGFALL = "Fall";
+	public static final String COURSEOFFERINGSPRING = "Spring";
+	public static final String COURSEOFFERINGSUMMER = "Summer";
+}
---
+++ CourseRecommendationSystem/src/courseRecommendationSystem/CourseLoader.java
@@ -0,0 +1,47 @@
+package courseRecommendationSystem;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+/**
+ * A class for loading the JSON file for Course
+ * @author Dongyu
+ *
+ */
+public class CourseLoader extends CourseConstants{
+	/**
+	 * A method which loads the course attributes from a JSON record
+	 * @return courses, an arrayList
+	 */
+	public static ArrayList<Course> loadCourse() {
+		ArrayList<Course> courses = new ArrayList<Course>();
+		ArrayList<Course> prerequisites = new ArrayList<Course>();
+		ArrayList<Course> corequisites = new ArrayList<Course>();
+		try {
+			FileReader reader = new FileReader(FILE_NAME);
+			JSONParser parser = new JSONParser();
+			JSONArray coursesJSON = (JSONArray)new JSONParser().parse(reader);
+			for(int i=0; i < coursesJSON.size(); i++){
+				JSONObject courseJSON = (JSONObject)coursesJSON.get(i);
+				String name = (String)courseJSON.get(NAME);
+				String code = (String)courseJSON.get(CODE);
+				int credit = (int)courseJSON.get(CREDITS);
+				prerequisites = (ArrayList<Course>) courseJSON.get(PREREQ);
+				corequisites = (ArrayList<Course>) courseJSON.get(COREQ);
+				boolean courseOfferingFall = (boolean)courseJSON.get(COURSEOFFERINGFALL);
+				boolean courseOfferingSpring = (boolean)courseJSON.get(COURSEOFFERINGSPRING);
+				boolean courseOfferingSummer = (boolean)courseJSON.get(COURSEOFFERINGSUMMER);
+				//Doesn't work with Course constructor
+				//courses.add(new Course(name, code, credit,prerequisites,corequisites,courseOfferingFall,courseOfferingSpring,courseOfferingSummer));
+			}
+			return courses;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
