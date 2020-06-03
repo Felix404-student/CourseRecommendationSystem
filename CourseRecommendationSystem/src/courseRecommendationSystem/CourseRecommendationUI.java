906008fa5fa4faf6e7a94238ee81bc5863eebb97
Felix404-student <justhbrown@gmail.com>
2020-06-03 10:58:33 -0400

    Added new UI class

--- CourseRecommendationSystem/src/courseRecommendationSystem/CourseRecommendationUI.java
+++ CourseRecommendationSystem/src/courseRecommendationSystem/CourseRecommendationUI.java
@@ -1,5 +1,13 @@
 package courseRecommendationSystem;
 
+/**
+ * This will be the User Interface class
+ * Functionality will be moved here from the overstuffed driver
+ * @author justinbrown
+ */
 public class CourseRecommendationUI {
-
+	
+	public CourseRecommendationUI() {
+		
+	}
 }
